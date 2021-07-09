f637fa8fe57217ada21de8ca4ceb3cc88e127a82
qwe813874700 <813874700@qq.com>
2021-07-09 18:08:17 +0800

    新增个图片~

---
+++ frontend/src/components/Topic/list.js
@@ -0,0 +1,18 @@
+export const topicTypeList = [
+  {
+    id: -1,
+    name: '全部'
+  },
+  {
+    id: 0,
+    name: '精华'
+  },
+  {
+    id: 1,
+    name: '分享'
+  },
+  {
+    id: 2,
+    name: '问答'
+  }
+]
